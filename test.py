459f5a9e5482e8dce20c28e40db94e65e7fcf0cf
Karl Menzel <menzelk@reed.edu>
2016-10-24 15:45:00 -0700

    switched to correct debuging file

---
+++ test.py
@@ -0,0 +1,21 @@
+
+
+from utils import *
+
+graph = readIn('toyGraph.txt')
+
+nodes, edges = graphToLists(graph)
+
+adj = graphToAdjList(graph)
+
+for i in adj:
+    print adj[i]
+'''
+for i in nodes:
+    print i
+
+for i in edges:
+    print i, graph[i[0]][i[1]]['weight']
+'''
+
+#CC = BFS(graph, 'A')
